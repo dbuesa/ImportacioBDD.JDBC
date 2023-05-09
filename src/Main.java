c0266657fe7d6d29af019c62e59c37477e325f5d
David Buesa <d.buesa@sapalomera.cat>
2023-05-09 10:05:26 +0200

    Modificació DAO

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import Exercicis.objectes.Candidatura;
 import Exercicis.objectes.ComunitatAutonoma;
+import Exercicis.objectes.DAO.CandidaturaDAODB;
 import Exercicis.objectes.DAO.ComunitatAutonomaDAODB;
 import Exercicis.objectes.DAO.PersonaDAODB;
 import Exercicis.objectes.Persona;
@@ -21,5 +23,18 @@ public class Main {
         }catch (SQLException ex){
             System.out.println(ex.getMessage());
         }
+
+        Candidatura can1 = new Candidatura(1, "123456", "BUGS", "Burundanda unida ganando sida", "123456", "123456", "123456");
+        CandidaturaDAODB can1DAO = new CandidaturaDAODB();
+        try {
+            if (can1DAO.create(can1)) {
+                System.out.println("Agregat!");
+            } else {
+                System.out.println("Oops... Algu ha sortit malament, maco...");
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
+
