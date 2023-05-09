4364a7c2393902bed05ca24427a7daa6b38aa34e
David Buesa <d.buesa@sapalomera.cat>
2023-05-09 20:00:24 +0200

    Reads done

--- src/Main.java
+++ src/Main.java
@@ -11,71 +11,9 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-
+        Vista.menu();
     }
 }
 
-
-        /*
-        ComunitatAutonomaDAODB caDAO = new ComunitatAutonomaDAODB();
-
-        try {
-            String infoComunitatAutonoma = caDAO.read(1);
-            System.out.println(infoComunitatAutonoma);
-
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-         */
-
-    /*
-        CandidaturaDAODB cDAODB = new CandidaturaDAODB();
-
-        try {
-            String infoCandidatura = cDAODB.read(1);
-            System.out.println(infoCandidatura);
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        PersonaDAODB pDAOB = new PersonaDAODB();
-
-        try {
-            String infoPersona = pDAOB.read(1);
-            System.out.println(infoPersona);
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-     */
-
-/*
-        Persona p1 = new Persona("Paco", "Perez", "Perez", "88465939");
-        PersonaDAODB p1DAO = new PersonaDAODB();
-
-        try {
-            if (p1DAO.create(p1)) {
-                System.out.println("Agregat, rei!");
-            } else {
-                System.out.println("Oops... Algu ha sortit malament, maco...");
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        Candidatura can1 = new Candidatura(1, "123456", "BUGS", "Burundanda unida ganando sida", "123456", "123456", "123456");
-        CandidaturaDAODB can1DAO = new CandidaturaDAODB();
-        try {
-            if (can1DAO.create(can1)) {
-                System.out.println("Agregat!");
-            } else {
-                System.out.println("Oops... Algu ha sortit malament, maco...");
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-
- */
 
 
