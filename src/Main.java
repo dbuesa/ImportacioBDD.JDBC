9f59c9b1be10b2578395631eda6fd68662709aea
David Buesa <d.buesa@sapalomera.cat>
2023-05-09 20:01:15 +0200

    Reads done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-import Exercicis.objectes.Candidatura;
-import Exercicis.objectes.ComunitatAutonoma;
-import Exercicis.objectes.DAO.CandidaturaDAODB;
-import Exercicis.objectes.DAO.ComunitatAutonomaDAODB;
-import Exercicis.objectes.DAO.PersonaDAODB;
-import Exercicis.objectes.Persona;
 import Exercicis.vista.Vista;
 
-import java.sql.SQLException;
-
 public class Main {
-
     public static void main(String[] args) {
         Vista.menu();
     }
