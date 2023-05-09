71483c7a9fccac178ff73a037825c532e9a3b24e
David Buesa <d.buesa@sapalomera.cat>
2023-05-09 08:57:36 +0200

    Create ComunitatAutonomaDAODB

--- src/Exercicis/objectes/DAO/DAODB.java
+++ src/Exercicis/objectes/DAO/DAODB.java
@@ -1,7 +1,9 @@
 package Exercicis.objectes.DAO;
 
+import java.sql.SQLException;
+
 public interface DAODB<T> {
-    boolean create(T t);
+    boolean create(T t) throws SQLException;
     boolean read(T t);
     boolean update(T t);
     boolean delete(T t);
