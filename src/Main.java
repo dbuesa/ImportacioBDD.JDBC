6523eb3f80bd28d91c65016ee491419f50f0584f
David Buesa <d.buesa@sapalomera.cat>
2023-05-10 11:29:42 +0200

    Deletes done

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
-import Exercicis.objectes.Candidatura;
-import Exercicis.objectes.ComunitatAutonoma;
-import Exercicis.objectes.DAO.CandidaturaDAODB;
-import Exercicis.objectes.DAO.ComunitatAutonomaDAODB;
-import Exercicis.objectes.DAO.PersonaDAODB;
-import Exercicis.objectes.Persona;
 import Exercicis.vista.Vista;
 
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
